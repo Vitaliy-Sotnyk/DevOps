22b0ba5ea731726f327e5fd4a8131b4e0b97b865
Vitaliy-Sotnyk <55446846+Vitaliy-Sotnyk@users.noreply.github.com>
2021-05-26 21:04:28 +0300

    Add files via upload

---
+++ js/generator_game.js
@@ -0,0 +1,218 @@
+function found(){
+    var type0=document.getElementById('s_g_0').value;
+    var type1=document.getElementById('s_g_1').value;
+    var type2=document.getElementById('s_g_2').value;
+    var type3=document.getElementById('s_g_3').value;
+    var type4=document.getElementById('s_g_4').value;
+    var type5=document.getElementById('s_g_5').value;
+    var type6=document.getElementById('s_g_6').value;
+    var type7=document.getElementById('s_g_7').value;
+    var type8=document.getElementById('s_g_8').value;
+    var type9=document.getElementById('s_g_9').value;
+    var type10=document.getElementById('s_g_10').value;
+    if(type0=="value2"){
+            if(type1=="value2"){
+               if(type2=="value2"){
+                    if(type3=="value24" || type3=="value47" || type3=="value5" ||  type3=="value4" || type3=="value25" || type3=="value14" || type3=="value42" || type3=="value48" || type3=="value29" || type3=="value22" || type3=="value10"){
+                        if(type4=="value24" || type4=="value47" || type4=="value5" ||  type4=="value4" || type4=="value25" || type4=="value14" || type4=="value42" || type4=="value48" || type4=="value29" || type4=="value22" || type4=="value10"){
+                            if(type3==type4){
+                                alert("Варіанти співпадають");
+                            }
+                            else{
+                                if(type5=="value24" || type5=="value47" || type5=="value5" ||  type5=="value4" || type5=="value25" || type5=="value14" || type5=="value42" || type5=="value48" || type5=="value29" || type5=="value22" || type5=="value10"){
+                                    if(type5==type4 || type5==type3){
+                                        alert("Варіанти співпадають");
+                                    }
+                                    else{
+                                        if(type6=="value24" || type6=="value47" || type6=="value5" ||  type6=="value4" || type6=="value25" || type6=="value14" || type6=="value42" || type6=="value48" || type6=="value29" || type6=="value22" || type6=="value10"){
+                                            if(type6==type5 || type6==type4 || type6==type3){                               
+                                                    alert("Варіанти співпадають");
+                                            }
+                                            else{
+                                                if(type7=="value1" || type7=="value47" || type7=="value5" ||  type7=="value4" || type7=="value25" || type7=="value14" || type7=="value42" || type7=="value48" || type7=="value29" || type7=="value22" || type7=="value10"){
+                                                    if(type7==type6 || type7==type5 || type7==type4 || type7==type3){
+                                                        alert("Варіанти співпадають");
+                                                    }
+                                                    else{
+                                                        if(type8=="value1" || type8=="value24" || type8=="value47" || type8=="value5" ||  type8=="value4" || type8=="value25" || type8=="value14" || type8=="value42" || type8=="value48" || type8=="value29" || type8=="value22" || type8=="value10"){
+                                                            if(type8==type7 ||type8==type6 || type8==type5 || type8==type4 || type8==type3){
+                                                                if(type8=="value1"|| type7=="value1"){
+                                                                    window.open('../games/The_Elder_Scrolls_V_Skyrim.html', '_blank');
+                                                                }
+                                                                else{
+                                                                    alert("Варіанти співпадають");
+                                                                }
+                                                            }
+                                                            else{
+                                                                if(type9=="value1" || type9=="value24" || type9=="value47" || type9=="value5" ||  type9=="value4" || type9=="value25" || type9=="value14" || type9=="value42" || type9=="value48" || type9=="value29" || type9=="value22" || type9=="value10"){
+                                                                    if(type9==type8 || type9==type7 ||type9==type6 || type9==type5 || type9==type4 || type9==type3){
+                                                                        if(type9=="value1" || type8=="value1"|| type7=="value1"){
+                                                                            window.open('../games/The_Elder_Scrolls_V_Skyrim.html', '_blank');
+                                                                        }
+                                                                        else{
+                                                                            alert("Варіанти співпадають");
+                                                                        }
+                                                                    }
+                                                                    else{
+                                                                        if(type10=="value1" || type10=="value24" || type10=="value47" || type10=="value5" ||  type10=="value4" || type10=="value25" || type10=="value14" || type10=="value42" || type10=="value48" || type10=="value29" || type10=="value22" || type10=="value10"){
+                                                                            if(type10==type9 || type10==type8 || type10==type7 ||type10==type6 || type10==type5 || type10==type4 || type10==type3){
+                                                                                if(type10=="value1" || type9=="value1" || type8=="value1"|| type7=="value1"){
+                                                                                    window.open('../games/The_Elder_Scrolls_V_Skyrim.html', '_blank');
+                                                                                }
+                                                                                else{
+                                                                                    alert("Варіанти співпадають");
+                                                                                }
+                                                                            }
+                                                                            else{
+                                                                                window.open('../games/The_Elder_Scrolls_V_Skyrim.html', '_blank');
+                                                                            }
+                                                                        } 
+                                                                        else{
+                                                                            alert("Варіант не найдено");
+                                                                        }                                                                    
+                                                                    }
+                                                                } 
+                                                                else{
+                                                                    alert("Варіант не найдено");
+                                                                }
+                                                            }
+                                                        } 
+                                                        else{
+                                                            alert("Варіант не найдено");
+                                                        }
+                                                    }
+                                                } 
+                                                else{
+                                                    alert("Варіант не найдено");
+                                                }
+                                            }
+                                        } 
+                                        else{
+                                            alert("Варіант не найдено");
+                                        }
+                                    }
+                                } 
+                                else{
+                                    alert("Варіант не найдено");
+                                }
+                            }
+                        } 
+                        else{
+                            alert("Варіант не найдено");
+                        }
+                    } 
+                    else{
+                        alert("Варіант не найдено");
+                    }
+                } 
+                else{
+                    alert("Варіант не найдено");
+                }
+            }       
+            else if(type1=="value3" || type1=="value4"){
+                if(type2=="value2"){
+                    if(type3=="value24" || type3=="value47" || type3=="value5" ||  type3=="value4" || type3=="value25" || type3=="value14" || type3=="value42" || type3=="value48" || type3=="value29" || type3=="value22" || type3=="value10"){
+                        if(type4=="value24" || type4=="value47" || type4=="value5" ||  type4=="value4" || type4=="value25" || type4=="value14" || type4=="value42" || type4=="value48" || type4=="value29" || type4=="value22" || type4=="value10"){
+                            if(type4==type3){
+                                    alert("Варіанти співпадають");
+                            }
+                            else{
+                                if(type5=="value1" || type5=="value24" || type5=="value47" || type5=="value5" || type5=="value4" || type5=="value25" || type5=="value14" || type5=="value42" || type5=="value48" || type5=="value29" || type5=="value22" || type5=="value10"){
+                                    if(type5==type4 || type5==type3){    
+                                            alert("Варіанти співпадають");
+                                    }
+                                    else{
+                                        if(type6=="value24" || type6=="value47" || type6=="value5" ||  type6=="value4" || type6=="value25" || type6=="value14" || type6=="value42" || type6=="value48" || type6=="value29" || type6=="value22" || type6=="value10"){
+                                                if(type6==type5 || type6==type4 || type6==type3){                               
+                                                        alert("Варіанти співпадають");
+                                                }
+                                                else{                                                
+                                                    if(type7=="value1" || type7=="value47" || type7=="value5" ||  type7=="value4" || type7=="value25" || type7=="value14" || type7=="value42" || type7=="value48" || type7=="value29" || type7=="value22" || type7=="value10"){
+                                                        if(type7==type6 || type7==type5 || type7==type4 || type7==type3){
+                                                            alert("Варіанти співпадають");
+                                                        }
+                                                        else{
+                                                            if(type8=="value1" || type8=="value24" || type8=="value47" || type8=="value5" ||  type8=="value4" || type8=="value25" || type8=="value14" || type8=="value42" || type8=="value48" || type8=="value29" || type8=="value22" || type8=="value10"){
+                                                                if(type8==type7 ||type8==type6 || type8==type5 || type8==type4 || type8==type3){
+                                                                    if(type8=="value1"|| type7=="value1"){
+                                                                        window.open('../games/The_Elder_Scrolls_online.html', '_blank');
+                                                                    }
+                                                                    else{
+                                                                        alert("Варіанти співпадають");
+                                                                    }
+                                                                }
+                                                                else{
+                                                                  if(type9=="value1" || type9=="value24" || type9=="value47" || type9=="value5" ||  type9=="value4" || type9=="value25" || type9=="value14" || type9=="value42" || type9=="value48" || type9=="value29" || type9=="value22" || type9=="value10"){
+                                                                        if(type9==type8 || type9==type7 ||type9==type6 || type9==type5 || type9==type4 || type9==type3){
+                                                                            if(type9=="value1" || type8=="value1"|| type7=="value1"){
+                                                                                window.open('../games/The_Elder_Scrolls_online.html', '_blank');
+                                                                            }
+                                                                            else{
+                                                                                alert("Варіанти співпадають");
+                                                                            }
+                                                                        }
+                                                                        else{
+                                                                            if(type10=="value1" || type10=="value24" || type10=="value47" || type10=="value5" ||  type10=="value4" || type10=="value25" || type10=="value14" || type10=="value42" || type10=="value48" || type10=="value29" || type10=="value22" || type10=="value10"){
+                                                                                if(type10==type9 || type10==type8 || type10==type7 ||type10==type6 || type10==type5 || type10==type4 || type10==type3){
+                                                                                    if(type10=="value1" || type9=="value1" || type8=="value1"|| type7=="value1"){
+                                                                                        window.open('../games/The_Elder_Scrolls_online.html', '_blank');
+                                                                                    }
+                                                                                    else{
+                                                                                        alert("Варіанти співпадають");
+                                                                                    }
+                                                                                }
+                                                                                else{
+                                                                                    window.open('../games/The_Elder_Scrolls_online.html', '_blank');
+                                                                                }
+                                                                            } 
+                                                                            else{
+                                                                                alert("Варіант не найдено");
+                                                                            }                                                                 
+                                                                        }
+                                                                    } 
+                                                                    else{
+                                                                        alert("Варіант не найдено");
+                                                                    }
+                                                                }
+                                                            } 
+                                                            else{
+                                                                alert("Варіант не найдено");
+                                                            }
+                                                        }
+                                                    } 
+                                                    else{
+                                                        alert("Варіант не найдено");
+                                                    }
+                                                }
+                                            } 
+                                        else{
+                                            alert("Виберіть бульше варіантів");
+                                            }
+                                    }
+                                } 
+                                else{
+                                    alert("Виберіть бульше варіантів");
+                                }                                                                    
+                            }
+                        } 
+                        else{
+                            alert("Виберіть бульше варіантів");
+                        } 
+                    } 
+                    else{
+                        alert("Виберіть бульше варіантів");
+                    }
+                } 
+                else{
+                    alert("Виберіть бульше варіантів");
+                }
+            }
+            else{
+                alert("Виберіть бульше варіантів");
+            }
+    }  
+    else{
+        alert("Даних на даний момент немає");
+    }
+
+}
