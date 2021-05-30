4f8597afc52ba814d1c20330a6e4936022eb5d0c
Vitaliy-Sotnyk <55446846+Vitaliy-Sotnyk@users.noreply.github.com>
2021-05-30 17:36:46 +0300

    Add files via upload

--- games/Shadow_Tactics/reting.js
+++ games/Shadow_Tactics/reting.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     if(localStorage.getItem('v_b')!=null){
          v_b=localStorage.getItem('v_b');
-           document.getElementById('result_v_b').innerText=v_b; 
+           document.getElementById('result_v_b').innerText=v_b;  
        }
        if(localStorage.getItem('b')!=null){
          b=localStorage.getItem('b');
@@ -19,6 +19,15 @@ window.onload = function(){
          v_g=localStorage.getItem('v_g');
            document.getElementById('result_v_g').innerText=v_g; 
        }
+       if(localStorage.getItem('ssd')!=null){
+        ssd=localStorage.getItem('ssd')
+        document.getElementById("submit").style.display=ssd;
+      }
+      if(localStorage.getItem('rsd')!=null){
+       rsd=localStorage.getItem('rsd')
+       document.getElementById("reset").style.display=rsd;
+     }
+
 }
 function hide(){
     var submit=document.getElementById("submit");
@@ -31,6 +40,11 @@ function hide(){
         submit.style.display  ="block";
         reset.style.display ="none";
     }
+    ssd=submit.style.display;
+    rsd=reset.style.display;
+    localStorage.setItem('ssd', ssd);
+    localStorage.setItem('rsd', rsd);
+
 }
 function disabled(){  
 document.getElementById("radio-1").disabled = true;
@@ -52,26 +66,51 @@ var radio=document.getElementsByName('radio');
        r_v_b=++document.getElementById('result_v_b').innerText;
        localStorage.setItem('v_b', r_v_b); 
        disabled();
+     /*  $.ajax({
+           type:"POST",
+           url:"grow_v_b.php",
+           data:this.serialize()
+       })*/
    } 
    if (radio[1].checked) {
        r_b=++document.getElementById('result_b').innerText;
       localStorage.setItem('b', r_b);
       disabled();
+    /*  $.ajax({
+        type:"POST",
+        url:"grow_b.php",
+        data:this.serialize()
+    })*/
     }
    if (radio[2].checked) {
        r_s=++document.getElementById('result_s').innerText;
        localStorage.setItem('s', r_s); 
        disabled();
+     /*  $.ajax({
+        type:"POST",
+        url:"grow_s_s.php",
+        data:this.serialize()
+    })*/
     }
    if (radio[3].checked) {
        r_g=++document.getElementById('result_g').innerText;
        localStorage.setItem('g', r_g);
        disabled();
+   /*    $.ajax({
+        type:"POST",
+        url:"grow_g.php",
+        data:this.serialize()
+    })*/
     }
    if (radio[4].checked) {
        r_v_g=++document.getElementById('result_v_g').innerText;
       localStorage.setItem('v_g', r_v_g);
       disabled();
+    /*  $.ajax({
+        type:"POST",
+        url:"grow_v_g.php",
+        data:this.serialize()
+    })*/
    }
    hide();
 }
@@ -81,26 +120,51 @@ var radio=document.getElementsByName('radio');
        r_v_b=--document.getElementById('result_v_b').innerText;
        localStorage.setItem('v_b', r_v_b); 
        document.getElementById("radio-1").disabled = false;
+   /*    $.ajax({
+        type:"POST",
+        url:"decrease_v_b.php",
+        data:this.serialize()
+    })*/
    } 
    if (radio[1].checked) {
        r_b=--document.getElementById('result_b').innerText;
       localStorage.setItem('b', r_b);
       document.getElementById("radio-2").disabled = false;
+     /* $.ajax({
+        type:"POST",
+        url:"decrease_b.php",
+        data:this.serialize()
+    })*/
     }
    if (radio[2].checked) {
        r_s=--document.getElementById('result_s').innerText;
        localStorage.setItem('s', r_s); 
        document.getElementById("radio-3").disabled = false;
+    /*   $.ajax({
+        type:"POST",
+        url:"decrease_s_s.php",
+        data:this.serialize()
+    })*/
     }
    if (radio[3].checked) {
        r_g=--document.getElementById('result_g').innerText;
        localStorage.setItem('g', r_g);
        document.getElementById("radio-4").disabled = false;
+     /*  $.ajax({
+        type:"POST",
+        url:"decrease_g.php",
+        data:this.serialize()
+    })*/
     }
    if (radio[4].checked) {
        r_v_g=--document.getElementById('result_v_g').innerText;
       localStorage.setItem('v_g', r_v_g);
       document.getElementById("radio-5").disabled = false;
+    /*  $.ajax({
+        type:"POST",
+        url:"decrease_v_g.php",
+        data:this.serialize()
+    })*/
    }
    abled();
    hide();
---
+++ games/pubg/decrease_g.php
@@ -0,0 +1,9 @@
+<?php 
+    $path='good.txt';
+    $fp=fopen($path, 'r');
+    $c=fread($fp,2);
+    fclose($fp);
+    $fp=fopen($path,'w')
+    fwrite($fp, $c-1);
+    fclose($fp);
+    ?>
